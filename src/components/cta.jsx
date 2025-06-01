8ad669b9d11669c51a6080f54400028d55864bb8
daviderosso-data <daviderosso.data@gmail.com>
2025-06-01 09:50:44 +0200

    0.9.8.5

--- src/components/cta.jsx
+++ src/components/cta.jsx
@@ -3,15 +3,15 @@ export default function Cta() {
         <div className="flex flex-col lg:flex-row m-10">
       
             <div className="flex-1 mb:flex-none bg-teal-600/70 text-white p-8 flex flex-col items-center justify-center rounded-lg drop-shadow-xl/30 m-3 ">
-                  <h2 className="text-3xl font-body font-bold mb-4">Tutta la meditazione che ti serve</h2>
+                  <h2 className="text-3xl font-body font-bold mb-4 text-center">Tutta la meditazione che ti serve</h2>
         <p className="mb-6 text-xl text-center justify-center">Scopri i nostri percorsi meditativi, pensati per guidarti verso il tuo <b>equilibrio interiore.</b> 
             Esplora pratiche uniche che uniscono calma, consapevolezza e benessere, trasformando ogni <b> momento in un'opportunità</b> per ritrovare serenità e crescita personale.
         </p>
-         <a href="/meditazione" className="drop-shadow-xl bg-yellow-500 text-teal-700 px-8 py-3 text-2xl rounded-full font-semibold hover:bg-yellow-100 hover:scale-130 hover:-translate-y-1 transition mt-10">
+         <a href="/meditazione" className="drop-shadow-xl bg-yellow-500 text-teal-700 px-8 py-3 text-2xl rounded-full font-semibold text-center hover:bg-yellow-100 hover:scale-130 hover:-translate-y-1 transition mt-10">
 Scopri i percorsi</a>
                 </div>
-        <div className="flex-1 mb:flex-none p-3 flex flex-col items-center justify-cente drop-shadow-xl/30 ">
-            <img src="/assets/images/meditationCTA.png" alt="Meditazione" className="rounded-lg shadow-lg transition duration-1200 hover:scale-105 hover:rotate-3" />
+        <div className="flex-1  mb:flex-none p-3 flex flex-col items-center  justify-center drop-shadow-xl/30 ">
+            <img src="/assets/images/meditationCTA.png" alt="Meditazione" className=" rounded-lg shadow-lg transition duration-1200 hover:scale-105 hover:rotate-3" />
             
             </div>
         </div>
--- src/components/cta_customMeditation.jsx
+++ src/components/cta_customMeditation.jsx
@@ -3,10 +3,10 @@ export default function cta_customMeditation() {
         <div className="flex flex-col lg:flex-row m-10">
       
             <div className="flex-1 mb:flex-none bg-teal-600/70 text-white p-8 flex flex-col items-center justify-center rounded-lg drop-shadow-xl/30 m-3 ">
-                  <h2 className="text-3xl font-body font-bold mb-4">Personalizza la tua meditazione</h2>
-        <p className="mb-6 text-xl text-center justify-center">Scelgli il tuo tempo e medita, nessuna fretta, nessuna pressione. Oppure scegli tra i numerosi percorsi meditativi.
+                  <h2 className="text-3xl font-body font-bold mb-4 text-center">Personalizza la tua meditazione</h2>
+        <p className="mb-6 text-xl text-center justify-center">Scelgli il tuo tempo e <b>medita</b>, nessuna fretta, nessuna pressione. Oppure scegli tra i numerosi <b>percorsi</b> meditativi.
         </p>
-         <a href="/meditazionecustom" className="drop-shadow-xl bg-yellow-500 text-teal-700 px-8 py-3 text-2xl rounded-full font-semibold hover:bg-yellow-100 hover:scale-130 hover:-translate-y-1 transition mt-10">
+         <a href="/meditazionecustom" className=" text-center drop-shadow-xl bg-yellow-500 text-teal-700 px-8 py-3 text-2xl rounded-full font-semibold hover:bg-yellow-100 hover:scale-130 hover:-translate-y-1 transition mt-10">
 Meditazione Personalizzata</a>
                 </div>
         <div className="flex-1 mb:flex-none p-3 flex flex-col items-center justify-cente drop-shadow-xl/30 ">
